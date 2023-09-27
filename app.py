af2f92f0cd068aaa60f8f396852a1aed4f8cd3dd
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-09-27 13:57:20 -0400

    9/27/23

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-x = "this is a thing"
-y = x.split( )
-z = y[0]
-print(y)
-print(z)
+#set stopwatch
+#if current time has 5 in a minute spot eastward false westward true
+#if current time has 0 in the minute spot eastward true westward false
+EastboundTraffic = input("True or false")
+WestboundTraffic = input("True or false")
+
+if input: "true" in EastboundTraffic 
+Westboundtraffic = False
+
