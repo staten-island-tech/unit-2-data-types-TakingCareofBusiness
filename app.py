0d6af5d9c3478c83b8bceb5bb7b77fdc06d526e2
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-09-28 14:06:42 -0400

    9/28/23

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 #set stopwatch
 #if current time has 5 in a minute spot eastward false westward true
 #if current time has 0 in the minute spot eastward true westward false
-EastboundTraffic = input("True or false")
-WestboundTraffic = input("True or false")
 
-if input: "true" in EastboundTraffic 
-Westboundtraffic = False
 
+
+x = 1123
+y = False
+def truthy(x,y):
+    if type(x) != bool or type(y) != bool:
+        type ("Glitch")
+    else:
+        if x == y:
+            print("false")
+        elif x != y:
+            print("true")
+truthy(x,y)
