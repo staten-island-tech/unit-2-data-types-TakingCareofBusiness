e67e46d66b7eff01fc4635e16cda18937cbc56f6
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-10-03 14:07:15 -0400

    10/3/23

--- gcf.py
+++ gcf.py
@@ -1,2 +1,22 @@
-number1 = input("Input first number here ")
-number2 = input("Input second number here ")
+number1 = int(input("Input first number here "))
+number2 = int(input("Input second number here "))
+
+a = 1
+def factor1(b):
+    for i in range(number1):
+        a = number1 % b
+        if a == 0:
+#0 is the remainder
+            print(b)
+        b += 1
+factor1(1)
+c = 3
+def factor2(d):
+    for i in range(number2):
+        c = number2 % d
+        if c == 0:
+#0 is the remainder
+            print(d)
+        d += 1
+factor2(1)
+    
