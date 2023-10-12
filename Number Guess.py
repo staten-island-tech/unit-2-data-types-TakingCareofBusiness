c2f5a9e16ac84dda3f92e5f89335bcf43c18bed3
TakingCareofBusiness <superspongysquid@yahoo.com>
2023-10-12 14:02:40 -0400

    Create Number Guess.py

---
+++ Number Guess.py
@@ -0,0 +1,9 @@
+import random
+randomnumber = random.randint(1, 10)
+guessnumber = input("Guess the number here ")
+def Bombtrack():
+    for i in range(11):
+        if guessnumber == randomnumber:
+            break
+        elif guessnumber == input("Guess the number here ")
+Bombtrack()
