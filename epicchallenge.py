21018adca64f503efe0847c319d1699ecf893c14
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-11-08 21:14:50 -0500

    huh

--- epicchallenge.py
+++ epicchallenge.py
@@ -1,5 +1,5 @@
 import random
-nouns = ["Michael", "Girgis", "Brandon Cubero", "Willie Caulie-Stein",]
+nouns = ["Michael", "Girgis", "Brandon Cubero", "Willie Caulie-Stein", ]
 adjectives = ["gargantuan", "volomptuous", "quickly", "carelessly", "smelly", "green",]
 verbs = ["Soloing the verse", "flossing", "rizzing",]
 def wordPicker(list1, list2, list3):
@@ -10,7 +10,7 @@ def wordPicker(list1, list2, list3):
         adjective1 = random.choice(list2)
         verb1 = random.choice(list3)
         verb2 = random.choice(list3)
-        print(noun1," went to town", verb1, "on a", noun2, verb2, "a feather in his cap And called it", adjective1, "macaroni.")
+        print(noun1," went to town", verb1, "on a", noun2, ",", verb2, "a feather in his cap And called it", adjective1, "macaroni.")
         answer = input("Would you like to generate a new Mad Lib? ")
         if (answer == "no") or (answer == "No"):
             again = False
